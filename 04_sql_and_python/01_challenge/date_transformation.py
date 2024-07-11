72a770e6cab92ee4ebba0553417220a8487c2da0
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-07-11 23:34:44 +0900

    Finish challenge 04-01

--- 04_sql_and_python/01_challenge/date_transformation.py
+++ 04_sql_and_python/01_challenge/date_transformation.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import re
 
 def transform_date_format(date_str):
     """
@@ -13,7 +14,19 @@ def transform_date_format(date_str):
     文字列: 'DD/MM/YYYY' 形式に変換した日付文字列、または元の文字列 (形式が無効だった場合)
     """
     # 日付形式を変換するロジックを実装してください
-    pass
+    date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}')
+    if date_pattern.match(date_str):
+        try:
+            values = date_str.split('-')
+            month = int(values[1])
+            day = int(values[2])
+            if 1 <= month <= 12 and 1 <= day <= 31:
+                values.reverse()
+                return '/'.join(values)
+        except:
+            pass
+    return date_str
+
 
 def apply_date_transformation(db_path, table_name, column_name):
     """
@@ -31,7 +44,10 @@ def apply_date_transformation(db_path, table_name, column_name):
         # 指定したテーブルをDataFrameに読み込んでください
         # 指定した列にtransform_date_format関数を適用してください
         # 変更後のDataFrameを返してください
-        pass
+        query = f"SELECT {column_name} FROM {table_name}"
+        df = pd.read_sql(query, conn)
+        return df.applymap(transform_date_format)
+
 
 # 使用例 (受講者はこれを変更してください)
 if __name__ == "__main__":
